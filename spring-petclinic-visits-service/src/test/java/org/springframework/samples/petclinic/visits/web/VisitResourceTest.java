edff8f082c77854a077f93e0ece9311676723b43
JonasAugust12 <cas.nmtoan@gmail.com>
2025-03-12 19:04:31 +0700

    abc

--- spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
@@ -57,6 +57,5 @@ class VisitResourceTest {
             .andExpect(jsonPath("$.items[0].petId").value(111))
             .andExpect(jsonPath("$.items[1].petId").value(222))
             .andExpect(jsonPath("$.items[2].petId").value(222));
-            //
     }
 }
